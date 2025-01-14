ee7c3a490f18fddd16cf6e931b46bc0eaa33587d
Mauro Manuel <mauro.manuel@unitel.co.ao>
2025-01-14 12:32:35 +0100

    added interface CategoryService and CategoryServiceImpl

--- src/main/java/com/masprogtech/services/CategoryService.java
+++ src/main/java/com/masprogtech/services/CategoryService.java
@@ -1,4 +1,11 @@
 package com.masprogtech.services;
 
+
+import com.masprogtech.dtos.CategoryDTO;
+
+import java.util.List;
+
 public interface CategoryService {
+
+    List<CategoryDTO> getAllCategory();
 }
--- src/main/java/com/masprogtech/services/impl/CategoryServiceImpl.java
+++ src/main/java/com/masprogtech/services/impl/CategoryServiceImpl.java
@@ -1,4 +1,39 @@
 package com.masprogtech.services.impl;
 
-public class CategoryServiceImpl {
+import com.masprogtech.dtos.CategoryDTO;
+import com.masprogtech.entities.Category;
+import com.masprogtech.repositories.CategoryRepository;
+import com.masprogtech.services.CategoryService;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    public CategoryServiceImpl(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    @Override
+    public List<CategoryDTO> getAllCategory() {
+
+        List<Category> categories = categoryRepository.findAll();
+        List<CategoryDTO> categoriesDTOs = new ArrayList<>();
+
+        for (Category category : categories) {
+            CategoryDTO categoryDTO = new CategoryDTO(
+                    category.getId(),
+                    category.getName()
+            );
+
+            categoriesDTOs.add(categoryDTO);
+        }
+
+        return categoriesDTOs;
+
+    }
 }
