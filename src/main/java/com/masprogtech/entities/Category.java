c64875f0ce78883970103922594bfe804ffcb782
Mauro Manuel <mauro.manuel@unitel.co.ao>
2025-01-14 12:29:20 +0100

    feat: added entity Category

--- src/main/java/com/masprogtech/entities/Category.java
+++ src/main/java/com/masprogtech/entities/Category.java
@@ -1,4 +1,88 @@
 package com.masprogtech.entities;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+
+import java.time.LocalDateTime;
+
+
+@Entity
+@Table(name = "tb_categories")
 public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(min = 5, message = "Category name must contain at least 5 characters")
+    private String name;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss")
+    private LocalDateTime createdAt;
+
+    @LastModifiedBy
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss")
+    private LocalDateTime updateAt;
+
+
+    public Category() {
+    }
+
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public @NotBlank @Size(min = 5, message = "Category name must contain at least 5 characters") String getName() {
+        return name;
+    }
+
+    public void setName(@NotBlank @Size(min = 5, message = "Category name must contain at least 5 characters") String name) {
+        this.name = name;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(LocalDateTime updateAt) {
+        this.updateAt = updateAt;
+    }
+
+    @PrePersist
+    protected void prePersist(){
+        if(this.createdAt == null)
+            createdAt = LocalDateTime.now();
+        if(this.updateAt == null)
+            updateAt = LocalDateTime.now();
+    }
+    @PreUpdate
+    protected void preUpdate(){
+        this.updateAt = LocalDateTime.now();
+    }
+
+
 }
