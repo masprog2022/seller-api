cbbe8b7bed4ac8049ff6801ab1c4c02a8a486fed
Mauro Manuel <mauro.manuel@unitel.co.ao>
2025-01-14 12:31:01 +0100

    feat: added CategoryRepository

--- src/main/java/com/masprogtech/repositories/CategoryRepository.java
+++ src/main/java/com/masprogtech/repositories/CategoryRepository.java
@@ -1,4 +1,10 @@
 package com.masprogtech.repositories;
 
-public interface CategoryRepository {
+import com.masprogtech.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Category findByName(String categoryName);
 }
