287f91065ed839389716bf2bc7b0ca3ce0466da7
Mauro Manuel <mauro.manuel@unitel.co.ao>
2025-01-14 12:28:07 +0100

    config: documentation swagger configuration

--- src/main/java/com/masprogtech/config/SpringDocOpenApiConfig.java
+++ src/main/java/com/masprogtech/config/SpringDocOpenApiConfig.java
@@ -1,4 +1,30 @@
 package com.masprogtech.config;
 
+
+
+import io.swagger.v3.oas.models.*;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SpringDocOpenApiConfig {
+    @Bean
+    public OpenAPI openAPI(){
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API - SELLER")
+                        .version("v1")
+                        .description("API para testar github actions ci/cd pipeline")
+                        .termsOfService("https://about-mauro.netlify.app/")
+                        .license(
+                                new License()
+                                        .name("GNU GENERAL PUBLIC LICENSE")
+                                        .url("https://www.gnu.org/licenses/gpl-3.0.pt-br.html")
+                        )
+                        .contact(new Contact().name("Mauro Manuel").url("https://www.linkedin.com/in/mauro-manuel-522947b2/"))
+                );
+    }
 }
