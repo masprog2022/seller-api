a90974d22a53a8019fe9ab746d3e10b77289f59e
Mauro Manuel <mauro.manuel@unitel.co.ao>
2025-01-14 12:31:49 +0100

    feat: added CategoryController

--- src/main/java/com/masprogtech/controllers/CategoryController.java
+++ src/main/java/com/masprogtech/controllers/CategoryController.java
@@ -1,4 +1,38 @@
 package com.masprogtech.controllers;
 
+import com.masprogtech.dtos.CategoryDTO;
+import com.masprogtech.services.CategoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/categories")
+@Tag(name = "Categoria", description = "Endpoints para gerenciar Categorias" )
 public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @Operation(summary = "Listar todas categorias", description = "Listar todas Categorias",
+
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Todos categorias listados com sucesso",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = CategoryDTO.class)))
+            })
+    @GetMapping("/all")
+    public ResponseEntity<List<CategoryDTO>> getAllCategories() {
+        return ResponseEntity.ok(categoryService.getAllCategory());
+    }
 }
