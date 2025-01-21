a1af31403f7d10c1b40e9f10b9a9317981b463f0
Mauro Manuel <mauro.manuel@unitel.co.ao>
2025-01-21 12:18:58 +0100

    fix: added fix CategoryDTO

--- src/main/java/com/masprogtech/dtos/CategoryDTO.java
+++ src/main/java/com/masprogtech/dtos/CategoryDTO.java
@@ -1,16 +1,30 @@
 package com.masprogtech.dtos;
 
 
+import java.time.LocalDateTime;
+
 public class CategoryDTO {
     private Long id;
     private String name;
+    private String description;
+    private LocalDateTime createdAt;
+    private LocalDateTime updateAt;
+
 
     public CategoryDTO() {
     }
 
-    public CategoryDTO(Long id, String name) {
+    public CategoryDTO(Long id, String name,  String description, LocalDateTime createdAt, LocalDateTime updateAt) {
         this.id = id;
         this.name = name;
+        this.description = description;
+        this.createdAt = createdAt;
+        this.updateAt = updateAt;
+    }
+
+    public CategoryDTO(String name,  String description) {
+        this.name = name;
+        this.description = description;
     }
 
     public Long getId() {
@@ -27,5 +41,29 @@ public class CategoryDTO {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(LocalDateTime updateAt) {
+        this.updateAt = updateAt;
     }
 }
