f5db437c8c1e763a00add540a3c2c79aa058d749
Mauro Manuel <mauro.manuel@unitel.co.ao>
2025-01-22 15:56:11 +0100

    feat: added ProductRepository

--- src/main/java/com/masprogtech/repositories/ProductRepository.java
+++ src/main/java/com/masprogtech/repositories/ProductRepository.java
@@ -1,4 +1,10 @@
 package com.masprogtech.repositories;
 
-public class ProductRepository {
+import com.masprogtech.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    boolean existsByNameAndCategoryId(String name, Long categoryId );
 }
