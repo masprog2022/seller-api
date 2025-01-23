7705383ed0fa761390ec9286b8672e6c8d7cfc31
Mauro Manuel <mauro.manuel@unitel.co.ao>
2025-01-23 10:16:43 +0100

    feat: added getAllProducts on ProductService

--- src/main/java/com/masprogtech/services/ProductService.java
+++ src/main/java/com/masprogtech/services/ProductService.java
@@ -1,7 +1,13 @@
 package com.masprogtech.services;
 
 import com.masprogtech.dtos.ProductDTO;
+import com.masprogtech.payload.ProductResponse;
 
 public interface ProductService {
     ProductDTO addProduct(Long categoryId, ProductDTO productDto);
+
+    ProductResponse getAllProducts(Integer pageNumber, Integer pageSize,
+                                   String sortBy, String sortOrder,
+                                   String keyword, String category);
+
 }
