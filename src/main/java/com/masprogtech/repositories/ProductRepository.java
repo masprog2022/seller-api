d6bdfdda6a4529055eb24494bf2a33f5e0dce4b0
Mauro Manuel <mauro.manuel@unitel.co.ao>
2025-01-23 10:21:50 +0100

    fix: added existsByNameAndCategoryId on ProductRepository

--- src/main/java/com/masprogtech/repositories/ProductRepository.java
+++ src/main/java/com/masprogtech/repositories/ProductRepository.java
@@ -2,9 +2,10 @@ package com.masprogtech.repositories;
 
 import com.masprogtech.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
     boolean existsByNameAndCategoryId(String name, Long categoryId );
 }
