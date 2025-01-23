64534f3db680d659022b6bd1c6c90a694a866a8e
Mauro Manuel <mauro.manuel@unitel.co.ao>
2025-01-23 11:51:42 +0100

    feat: added MessageResponse

---
+++ src/main/java/com/masprogtech/payload/MessageResponse.java
@@ -0,0 +1,27 @@
+package com.masprogtech.payload;
+
+public class MessageResponse {
+    private String message;
+    private boolean success;
+
+    public MessageResponse(String message, boolean success) {
+        this.message = message;
+        this.success = success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+}
