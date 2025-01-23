b01e4618eb1b1d31f9db2dd4417e60fda6604cca
Mauro Manuel <mauro.manuel@unitel.co.ao>
2025-01-23 10:13:26 +0100

    feat: added getAllProducts on ProductServiceImpl

--- src/main/java/com/masprogtech/services/impl/ProductServiceImpl.java
+++ src/main/java/com/masprogtech/services/impl/ProductServiceImpl.java
@@ -5,10 +5,16 @@ import com.masprogtech.entities.Category;
 import com.masprogtech.entities.Product;
 import com.masprogtech.exceptions.APIException;
 import com.masprogtech.exceptions.ResourceNotFoundException;
+import com.masprogtech.payload.ProductResponse;
 import com.masprogtech.repositories.CategoryRepository;
 import com.masprogtech.repositories.ProductRepository;
 import com.masprogtech.services.ProductService;
 import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -30,7 +36,7 @@ public class ProductServiceImpl implements ProductService {
     @Override
     public ProductDTO addProduct(Long categoryId, ProductDTO productDto) {
         Category category = categoryRepository.findById(categoryId)
-                .orElseThrow(() -> new ResourceNotFoundException("Category", "categoryId", categoryId));
+                .orElseThrow(() -> new ResourceNotFoundException("Category", "id", categoryId));
 
            if (productRepository.existsByNameAndCategoryId(productDto.getName(), categoryId)) {
                throw new APIException("Product already exists!");
@@ -42,6 +48,52 @@ public class ProductServiceImpl implements ProductService {
             product.setCreatedAt(LocalDateTime.now());
             Product savedProduct = productRepository.save(product);
             return modelMapper.map(savedProduct, ProductDTO.class);
+    }
+
+    @Override
+    public ProductResponse getAllProducts(Integer pageNumber, Integer pageSize,
+                                          String sortBy, String sortOrder,
+                                          String keyword, String category) {
+
+        Sort sortByAndOrder = sortOrder.equalsIgnoreCase("asc")
+                ? Sort.by(sortBy).ascending()
+                : Sort.by(sortBy).descending();
+
+        Pageable pageDetails = PageRequest.of(pageNumber, pageSize, sortByAndOrder);
+        Specification<Product> spec = Specification.where(null);
+        if (keyword != null && !keyword.isEmpty()){
+            spec = spec.and((root, query, criteriaBuilder)
+                    -> criteriaBuilder.like(root.get("name"), "%" + keyword.toLowerCase() + "%"));
+        }
+
+        if (category != null && !category.isEmpty()) {
+            spec = spec.and((root, query, criteriaBuilder) ->
+                    criteriaBuilder.like(root.get("category").get("name"), category));
+        }
+       Page<Product> pageProducts = productRepository.findAll(spec, pageDetails);
+
+        List<Product> products = pageProducts.getContent();
+
+        List<ProductDTO> productDTOS = products.stream()
+                .map(product -> {
+                    ProductDTO dto = modelMapper.map(product, ProductDTO.class);
+                    if (product.getCategory() != null ){
+                        dto.setCategoryName(product.getCategory().getName());
+                    }
+                    return dto;
+                })
+                .toList();
+
+        ProductResponse productResponse = new ProductResponse();
+        productResponse.setContent(productDTOS);
+        productResponse.setPageNumber(pageProducts.getNumber());
+        productResponse.setPageSize(pageProducts.getSize());
+        productResponse.setTotalElements(pageProducts.getTotalElements());
+        productResponse.setTotalPages(pageProducts.getTotalPages());
+        productResponse.setLastPage(pageProducts.isLast());
+        return productResponse;
+
+
     }
 
     private double calculateSpecialPrice(double price, double discount) {
