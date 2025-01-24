c03017babd082ea38477bea201132aba3dd8c948
Mauro Manuel <mauro.manuel@unitel.co.ao>
2025-01-24 14:35:46 +0100

    feat: added entity OrderItem

--- src/main/java/com/masprogtech/entities/OrderItem.java
+++ src/main/java/com/masprogtech/entities/OrderItem.java
@@ -1,4 +1,24 @@
 package com.masprogtech.entities;
 
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_order_items")
 public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderItemId;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    private Integer quantity;
+    private Double price;
 }
