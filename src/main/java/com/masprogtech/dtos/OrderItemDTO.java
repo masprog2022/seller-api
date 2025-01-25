38868b04c1cd4d051424a1e9c3b2c31c7f93ffe1
Mauro Manuel <mauro.manuel@unitel.co.ao>
2025-01-25 12:32:28 +0100

    feat: add OrderItemDTO

--- src/main/java/com/masprogtech/dtos/OrderItemDTO.java
+++ src/main/java/com/masprogtech/dtos/OrderItemDTO.java
@@ -1,4 +1,40 @@
 package com.masprogtech.dtos;
 
 public class OrderItemDTO {
+    private Long productId;
+    private Integer quantity;
+    private Double price; // Opcional, pode ser calculado no backend
+
+    public OrderItemDTO() {
+    }
+
+    public OrderItemDTO(Long productId, Integer quantity, Double price) {
+        this.productId = productId;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
 }
