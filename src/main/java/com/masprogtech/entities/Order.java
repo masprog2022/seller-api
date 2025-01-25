c47b71e5da28924ef784b505e3db2286657446e5
Mauro Manuel <mauro.manuel@unitel.co.ao>
2025-01-25 12:33:54 +0100

    feat: add getters, setters and constructor

--- src/main/java/com/masprogtech/entities/Order.java
+++ src/main/java/com/masprogtech/entities/Order.java
@@ -25,4 +25,65 @@ public class Order {
     private OrderStatus status;
 
     private String address;
+
+    public Order() {
+    }
+
+    public Order(Long orderId, String customer, List<OrderItem> items,
+                 Double totalPrice, OrderStatus status, String address) {
+        this.orderId = orderId;
+        Customer = customer;
+        this.items = items;
+        this.totalPrice = totalPrice;
+        this.status = status;
+        this.address = address;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getCustomer() {
+        return Customer;
+    }
+
+    public void setCustomer(String customer) {
+        Customer = customer;
+    }
+
+    public List<OrderItem> getItems() {
+        return items;
+    }
+
+    public void setItems(List<OrderItem> items) {
+        this.items = items;
+    }
+
+    public Double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(Double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
--- src/main/java/com/masprogtech/entities/OrderItem.java
+++ src/main/java/com/masprogtech/entities/OrderItem.java
@@ -21,4 +21,55 @@ public class OrderItem {
 
     private Integer quantity;
     private Double price;
+
+    public OrderItem() {
+    }
+
+    public OrderItem(Long orderItemId, Product product, Order order, Integer quantity, Double price) {
+        this.orderItemId = orderItemId;
+        this.product = product;
+        this.order = order;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public Long getOrderItemId() {
+        return orderItemId;
+    }
+
+    public void setOrderItemId(Long orderItemId) {
+        this.orderItemId = orderItemId;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
 }
